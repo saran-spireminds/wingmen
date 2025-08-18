f508fe69c96c4eac5079b952727f9e83e93fa7c0
Saravanan <saravanan.thangavelup@gmail.com>
2025-08-18 16:10:52 +0530

    Updated the articles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -929,7 +929,7 @@ export default function Home() {
                        lg:max-w-[500px] xl:max-w-[800px] 2xl:max-w-[1250px] aspect-square"
           >
             <Image
-              src="/Partner with Wingmen.jpg"
+              src="/fundraising.jpg"
               alt="Partner with Wingmen Image"
               fill
               className="object-cover rounded-lg"
